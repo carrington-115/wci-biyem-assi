dadd22cfcaf3f17e7441e027b06f96e5be961155
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-28 00:49:10 +0530

    set the about us page top component file

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,28 +1,9 @@
-import TopPageComponent from "@/components/TopPageComponent";
+import AboutTopComponent from "@/components/about-components/AboutTopComponent";
 
 export default function Page() {
   return (
     <>
-      <TopPageComponent
-        image="/images/aboutus/top-image.svg"
-        children={
-          <div className="flex flex-col items-center text-white w-4/5">
-            <div className="flex items-end gap-5">
-              <span className="w-24 h-2 bg-white" />
-              <h1 className="font-bold text-5xl">The Mandate</h1>
-              <span className="w-24 h-2 bg-white" />
-            </div>
-            <blockquote className="text-2xl mt-3 font-semibold text-center">
-              "And now the Hour has come to liberate mankind from every
-              operation of the devil and I am sending you to undertake this task
-              through the preaching of the word of Faith."
-            </blockquote>
-          </div>
-        }
-      />
-      <div>
-        <h1>Hello world</h1>
-      </div>
+      <AboutTopComponent />
     </>
   );
 }
---
+++ src/components/about-components/AboutTopComponent.tsx
@@ -0,0 +1,25 @@
+import TopPageComponent from "../TopPageComponent";
+
+export default function AboutTopComponent() {
+  return (
+    <div>
+      <TopPageComponent
+        image="/images/aboutus/top-image.svg"
+        children={
+          <div className="flex flex-col items-center text-white w-4/5">
+            <div className="flex items-end gap-5">
+              <span className="w-24 h-2 bg-white" />
+              <h1 className="font-bold text-5xl">The Mandate</h1>
+              <span className="w-24 h-2 bg-white" />
+            </div>
+            <blockquote className="text-2xl mt-3 font-semibold text-center">
+              "And now the Hour has come to liberate mankind from every
+              operation of the devil and I am sending you to undertake this task
+              through the preaching of the word of Faith."
+            </blockquote>
+          </div>
+        }
+      />
+    </div>
+  );
+}
