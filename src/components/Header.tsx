bcbcef62af4253d3aa3a2bfcc200e1611b777fad
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-27 10:26:17 +0530

    added the functionality to change the header style based on the positon of the scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,34 @@ import Link from "next/link";
 import { tabLinks } from "./data/data";
 import { linkListType } from "./types/types";
 import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Header() {
   const pathname = usePathname();
   const router = useRouter();
+  const [scrollState, setScrollState] = useState<boolean>(false);
+  function setScrollChange() {
+    const position = window.scrollY;
+    if (position > 200) setScrollState(true);
+    else setScrollState(false);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", setScrollChange);
+
+    return () => {
+      window.removeEventListener("scroll", setScrollChange);
+    };
+  }, []);
+
   return (
-    <header className="fixed top-0 z-50 w-screen flex justify-around items-center pt-10 pb-5 bg-gradient-to-b from-black to-transparent">
+    <header
+      className={
+        scrollState
+          ? "fixed top-0 z-50 w-screen flex justify-around items-center py-5 bg-black"
+          : "fixed top-0 z-50 w-screen flex justify-around items-center pt-10 pb-5 bg-gradient-to-b from-black to-transparent"
+      }
+    >
       <span className="cursor-pointer" onClick={() => router.push("/")}>
         <img src="/images/logo.svg" alt="logo" />
       </span>
