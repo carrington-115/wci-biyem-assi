e184da27c88dd2a7385a03ed279e7c831825b568
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-28 10:57:34 +0530

    built the feature for overlay when the button is still on learn more

--- src/components/about-components/AboutArticle.tsx
+++ src/components/about-components/AboutArticle.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import { aboutArticleType } from "../types/types";
 
 export default function AboutArticle({
@@ -6,6 +8,29 @@ export default function AboutArticle({
   bodyContent,
   id,
 }: aboutArticleType) {
+  const [learnMore, setLearnMore] = useState<boolean>(false);
+  const [temporaryArticle, setTemporaryArticle] = useState<string[]>([]);
+
+  function handleLearnMoreAction() {
+    setLearnMore(true);
+  }
+
+  useEffect(() => {
+    if (!learnMore) {
+      for (let i = 0; i < bodyContent.length - 1; i++) {
+        setTemporaryArticle((prev) => {
+          return [...prev, bodyContent[i]];
+        });
+      }
+    } else {
+      for (let i = 0; i < bodyContent.length - 1; i++) {
+        setTemporaryArticle((prev) => {
+          return [...prev, bodyContent[i]];
+        });
+      }
+    }
+  }, []);
+
   return (
     <section className="w-full flex justify-between mb-10">
       <div className="w-1/3 flex flex-col gap-5">
@@ -15,13 +40,32 @@ export default function AboutArticle({
           </div>
         ))}
       </div>
-      <div className="w-3/5" id={id}>
+      <div className="w-3/5 relative" id={id}>
+        {!learnMore && (
+          <div className="absolute z-10 h-full w-full top-0 right-0 left-0 bottom-0 bg-gradient-to-t from-black via-transparent to-transparent flex justify-center items-end box-border pb-7">
+            <button
+              onClick={handleLearnMoreAction}
+              className="flex box-border bg-white text-black font-semibold px-6 py-1 rounded-full gap-2 justify-center items-center"
+            >
+              <p>Learn more</p>
+              <span>
+                <img src="/images/icons/learn-more.svg" alt="learn more icon" />
+              </span>
+            </button>
+          </div>
+        )}
         <h1 className="mb-10 font-bold text-2xl">{title}</h1>
-        {bodyContent.map((content) => (
-          <p className="text-justify font-medium mb-5">
-            &nbsp;&nbsp;&nbsp;&nbsp;{content}
-          </p>
-        ))}
+        {learnMore
+          ? bodyContent.map((content) => (
+              <p className="text-justify font-medium mb-5">
+                &nbsp;&nbsp;&nbsp;&nbsp;{content}
+              </p>
+            ))
+          : temporaryArticle.map((content) => (
+              <p className="text-justify font-medium mb-5">
+                &nbsp;&nbsp;&nbsp;&nbsp;{content}
+              </p>
+            ))}
       </div>
     </section>
   );
